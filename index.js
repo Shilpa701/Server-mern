c376f05a9bdd1679b4e0d148d8a4b03bd6d867fe
Shilpa701 <shilpasajeevan0101@gmail.com>
2025-03-14 13:02:43 +0530

    final

--- index.js
+++ index.js
@@ -17,11 +17,26 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true
-})); 
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   credentials: true
+// })); 
+
+const allowedOrigins = ['http://localhost:5173', 'https://server-mern-1-7v7o.onrender.com'];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  })
+);
 
 app.use('/auth',appRoute)
 app.use('/properties',listingRoutes)
